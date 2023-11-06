c0cd81399bc5c4619254616c32625e3c6e0e0bee
Dmitry Matyukhin <nskmda@gmail.com>
2023-11-05 20:32:26 -0500

    Minor code optimizations within FCPS limits,

--- Client.java
+++ Client.java
@@ -1,80 +1,56 @@
 import java.util.*;
-import java.io.*;
 
 public class Client{
-   public static void main(String[] args){
+
+   private static final String[] PLAYER_NAMES = {"Bob", "Joe", "Bill", "Kevin"};
+
+//   private static final int MAX_MOVES = 1000;
+
+   private static void nextMoveFor(Player player)
+   {
+      int spin = Board.spin();
+      System.out.println(player.getName() + " spins: " + spin);
+      player.setSpace(Board.getNewLocation(player.getSpace(), spin));
+      System.out.println(player + "\n\n");
+   }
+   public static void main(String[] args)
+   {
       Scanner input = new Scanner(System.in);
       System.out.println("How many players (2 - 4)?");
-      int plrNum = input.nextInt();
-      
-      Player p1 = new Player("Bob");
-      Player p2 = new Player("Joe");
-      Player p3 = new Player("Bill");
-      Player p4 = new Player("Kevin");
-      
-      Board b = new Board();
-      
-      while(p1.getSpace() < 100 && p2.getSpace() < 100 && p3.getSpace() < 100 && p4.getSpace() < 100){
-      
-         if(plrNum >= 1){
-            int spin = b.spin();
-            System.out.println(p1.getName() + " spins: " + spin);
-            int newLoc = b.getNewLocation(p1.getSpace(), spin);
-            p1.setSpace(newLoc);
-            System.out.println(p1.getName() + " is on space " + p1.getSpace());  
-            System.out.println("");
-            System.out.println("");
+      int gamePlayersNumber = input.nextInt();
+
+//      if (gamePlayersNumber>4 || gamePlayersNumber<2)
+//      {
+//         System.out.println("Number of players must be 2, 3 or 4.");
+//         System.out.println("Please, enter a valid number next time you start the game simulator.");
+//         return;
+//      }
+
+      Player[] players = new Player[gamePlayersNumber];
+      for (int i=0; i<gamePlayersNumber; i++)
+      { players[i] =  new Player(PLAYER_NAMES[i]); }
+
+      boolean gameEnded = false;
+//      int moveCount = 0;
+
+      while(!gameEnded)
+      {
+//         moveCount++;
+         for (int i=0; i<gamePlayersNumber; i++)
+         {
+            nextMoveFor(players[i]);
+            if (players[i].hasWon())
+            {
+               System.out.println(players[i].getName() + " wins!");
+               gameEnded = true;
+               break;
+            }
          }
-         
-         if(p1.getSpace() == 100){
-            System.out.println(p1.getName() + " wins!");
-            break;
-         }
-         
-         if(plrNum >= 2){
-            int spin = b.spin();
-            System.out.println(p2.getName() + " spins: " + spin);
-            int newLoc = b.getNewLocation(p2.getSpace(), spin);
-            p2.setSpace(newLoc);
-            System.out.println(p2.getName() + " is on space " + p2.getSpace());  
-            System.out.println("");
-            System.out.println("");
-         }
-         
-         if(p2.getSpace() == 100){
-            System.out.println(p2.getName() + " wins!");
-            break;
-         }
-         
-         if(plrNum >= 3){
-            int spin = b.spin();
-            System.out.println(p3.getName() + " spins: " + spin);
-            int newLoc = b.getNewLocation(p3.getSpace(), spin);
-            p3.setSpace(newLoc);
-            System.out.println(p3.getName() + " is on space " + p3.getSpace());  
-            System.out.println("");
-            System.out.println("");
-         }
-         
-         if(p3.getSpace() == 100){
-            System.out.println(p3.getName() + " wins!");
-            break;
-         }
-         
-         if(plrNum >= 4){
-            int spin = b.spin();
-            System.out.println(p4.getName() + " spins: " + spin);
-            int newLoc = b.getNewLocation(p4.getSpace(), spin);
-            p4.setSpace(newLoc);
-            System.out.println(p4.getName() + " is on space " + p4.getSpace());  
-            System.out.println("");
-            System.out.println("");
-         }     
-         
-         if(p4.getSpace() == 100){
-            System.out.println(p4.getName() + " wins!");
-            break;
-         }  
+//         if (MAX_MOVES <= moveCount && !gameEnded)
+//         {
+//            gameEnded = true;
+//            System.out.println("Everybody got tired. The game is not going anywhere. We go home!");
+//         }
       }
    }
 }
