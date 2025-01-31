f3d5b51f8c2403f65e1dd87d008c79a4c6dcf851
CatFoolYou <95507639+catfoolyou@users.noreply.github.com>
2025-01-31 16:26:48 -0500

    shittification

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -3,79 +3,90 @@ import java.util.*;
 
 public class PrimeNumbers{
 
-    private static int numbersPerLine = 10;
+    private static final int numbersPerLine = 10;
+    private static final int numbersPerRow = 5;
     
-        public static void main(String args[]) throws IOException{
-            System.out.println("Enter amount of prime numbers to generate:");
-            Scanner input = new Scanner(System.in);
+    public static void main(String args[]) throws IOException{
+        System.out.println("Enter amount of prime numbers to generate:");
+        Scanner input = new Scanner(System.in);
 
-            int amount = input.nextInt();
-    
-            generatePrimeNumbers(amount, "primeNumbers.txt");
-    
-            int[] primeNumbers = readNumbersIntoArray(amount, "primeNumbers.txt");
+        int amount = input.nextInt();
 
-            for (int i = 1; i < amount; i++) {
-                System.out.printf("%10s", primeNumbers[i]);
-                if(i % 10 == 0){
-                    System.out.println("");
-                }
+        generatePrimeNumbers(amount, "primeNumbers.txt");
+
+        ArrayList<Integer> primeNumbers = readNumbersIntoArray(amount, "primeNumbers.txt");
+
+        System.out.printf("\nThe first %s prime numbers (2 - %s)\n", amount, primeNumbers.get(primeNumbers.size() - 1));
+
+        for (int i = 0; i < amount; i++) {
+            System.out.printf("%10s", primeNumbers.get(i));
+            if((i + 1) % numbersPerRow == 0){
+                System.out.println("");
             }
-
-            System.out.println("\n");
-   
         }
-    
-        public static void generatePrimeNumbers(int amount, String destFile) throws IOException{
-            try {
-                new File(destFile).delete(); // Clear file (there is a better way to do this but I'm lazy)
-            } catch (Exception e) {
-            }
-    
-            BufferedWriter writer = new BufferedWriter(new FileWriter(destFile, true));
-            writer.append(amount + "\n");
-            ArrayList<Integer> primeNumbers = new ArrayList<>();
 
-            int numbersAdded = 0;
-            int num = 1;
+        System.out.println("\n");
+
+    }
     
-            while(numbersAdded < amount){
-                if(isPrime(num)){
-                    primeNumbers.add(num);
-                    numbersAdded ++;
-                }
-                num ++;
+    public static void generatePrimeNumbers(int amount, String destFile) throws IOException{
+        try {
+            new File(destFile).delete(); // Clear file (there is a better way to do this but I'm lazy)
+        } catch (Exception e) {
+        }
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(destFile, true));
+        writer.append(amount + "\n");
+        ArrayList<Integer> primeNumbers = new ArrayList<>();
+
+        int numbersAdded = 0;
+        int num = 1;
+
+        while(numbersAdded < amount){
+            if(isPrime(num)){
+                primeNumbers.add(num);
+                numbersAdded ++;
             }
-    
-            int count = 0;
-            for(int i = 0; i < primeNumbers.size(); i++){
-                String number = (primeNumbers.get(i) + "");
-                if(i < primeNumbers.size() - 1){
-                    number = number + " ";
-                }
-                writer.append(number); 
-                count ++;
-                if(count == numbersPerLine){
+            num ++;
+        }
+
+        int count = 0;
+
+        for(int i = 0; i < primeNumbers.size(); i++){
+            String number = (primeNumbers.get(i) + "");
+            writer.append(number);
+            count ++;
+            if(count < numbersPerLine){
+                writer.append(",");
+            }
+            if(count == numbersPerLine && i < primeNumbers.size() - 1){
                 writer.append("\n");
                 count = 0;
             }
-            
         }
 
         writer.close();
     }
 
-    public static int[] readNumbersIntoArray(int amount, String destFile) throws IOException {
-        Scanner reader = new Scanner(new FileReader(destFile));
+    public static ArrayList<Integer> readNumbersIntoArray(int amount, String srcFile) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(srcFile));
+        reader.readLine();
 
-        int[] primeNumbers = new int[amount + 1];
-        primeNumbers[0] = 100;
+        String row;
+        ArrayList<Integer> primeNumbers = new ArrayList<>();
+        String[] line;
 
-        for (int i = 0; i < amount; i++) {
-            primeNumbers[i] = Integer.parseInt(reader.next());
-            //System.out.println(Integer.parseInt(reader.next()));
+        ArrayList<Integer> numbersInLine = new ArrayList<>();
+
+        while((row = reader.readLine()) != null){
+            line = row.split(",");
+            numbersInLine.clear();
+            for(String s : line){
+                numbersInLine.add(Integer.parseInt(s));
+            }
+            primeNumbers.addAll(numbersInLine);
         }
-        
+
         return primeNumbers;
     }
 
